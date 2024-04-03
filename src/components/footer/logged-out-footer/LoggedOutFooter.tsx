ea04808ee25fd0e9382ffdd10bde8e4f74f21ee9
jusgri5 <justinas.gricius@gmail.com>
2024-04-03 10:38:05 +0300

    Fixed guest footer issues with showLabel prop

--- src/components/footer/logged-out-footer/LoggedOutFooter.tsx
+++ src/components/footer/logged-out-footer/LoggedOutFooter.tsx
@@ -15,10 +15,12 @@ const LoggedOutFooter: React.FC<LoggedOutFooterProps> = ({
 	return (
 		<BottomNavigation className='guest-footer' showLabels>
 			<BottomNavigationAction onClick={handleClickTerms} label='Terms of service' />
-			<div className='divider' />
 			<BottomNavigationAction onClick={handleClickSupport} label='Support' />
-			<div className='divider' />
-			<BottomNavigationAction onClick={handleClickCognizant} label={`© ${new Date().getFullYear()} Cognizant`} />
+			<BottomNavigationAction
+				className='last-button'
+				onClick={handleClickCognizant}
+				label={`© ${new Date().getFullYear()} Cognizant`}
+			/>
 		</BottomNavigation>
 	);
 };

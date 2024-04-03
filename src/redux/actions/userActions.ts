a3a486866f9819206fb1097bc4eb0d4ebc6ce535
Linas Sarpauskas <l.sarpauskas@gmail.com>
2024-04-03 16:44:29 +0300

    added validation for email and password input fields

--- src/redux/actions/userActions.ts
+++
@@ -1,8 +0,0 @@
-export const SET_EMAIL = 'SET_EMAIL';
-
-export const setEmail = (email: string) => ({
-  type: SET_EMAIL,
-  payload: email,
-});
-
-export type ActionTypes = ReturnType<typeof setEmail>;

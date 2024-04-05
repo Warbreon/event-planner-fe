e38cf0670f30fd911d763b0b5566a64dacbf75ee
Apozzz <bosas.arnas2000@gmail.com>
2024-04-05 10:41:50 +0300

    fix: fixed positions and margins.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import ROUTES from '../routes/routes';
@@ -19,20 +20,21 @@ import Main from '../pages/main/Main';
 function App() {
 	return (
 		<Routes>
-			<Route index element={<ExploreEvents />} />
-			<Route path={ROUTES.SIGN_IN} element={<SignIn />} />
-			<Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
-			<Route path={ROUTES.RESET_PASSWORD} element={<PasswordReset />} />
-			<Route path={ROUTES.EVENT} element={<Event />} />
-			<Route path={ROUTES.MY_EVENTS} element={<MyEvents />} />
-			<Route path={ROUTES.ADD_EVENT} element={<CreateEvent />} />
-			<Route path={ROUTES.EDIT_EVENT} element={<EditEvent />} />
-			<Route path={ROUTES.SETTINGS} element={<Settings />} />
-			<Route path={ROUTES.PRIVACY_POLICY} element={<PrivacyPolicy />} />
-			<Route path={ROUTES.TERMS_OF_SERVICE} element={<TermOfService />} />
-			<Route path={ROUTES.SUPPORT} element={<Support />} />
-			<Route path={ROUTES.NOT_FOUND} element={<PageNotFound />} />
-			{/* <Route path={ROUTES.INDEX} element={<Main />} /> */}
+			<Route path={ROUTES.INDEX} element={<Main />}>
+				<Route index element={<ExploreEvents />} />
+				<Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+				<Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
+				<Route path={ROUTES.RESET_PASSWORD} element={<PasswordReset />} />
+				<Route path={ROUTES.EVENT} element={<Event />} />
+				<Route path={ROUTES.MY_EVENTS} element={<MyEvents />} />
+				<Route path={ROUTES.ADD_EVENT} element={<CreateEvent />} />
+				<Route path={ROUTES.EDIT_EVENT} element={<EditEvent />} />
+				<Route path={ROUTES.SETTINGS} element={<Settings />} />
+				<Route path={ROUTES.PRIVACY_POLICY} element={<PrivacyPolicy />} />
+				<Route path={ROUTES.TERMS_OF_SERVICE} element={<TermOfService />} />
+				<Route path={ROUTES.SUPPORT} element={<Support />} />
+				<Route path={ROUTES.NOT_FOUND} element={<PageNotFound />} />
+			</Route>
 		</Routes>
 	);
 }

b2b941f3af83beb44db5290f3d99a611ac4f23bd
jusgri5 <justinas.gricius@gmail.com>
2024-04-22 17:14:45 +0300

    Using BE for explore page data, with temporary JWT token

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,10 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
+
+const tempToken =
+	'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndXkuaGF3a2luc0BnbWFpbC5jb20iLCJpYXQiOjE3MTM3OTQzODYsImV4cCI6MTcxMzg4MDc4Nn0._R4meTOryF5FEc9A1aRDXzjyJfd3klYl8GQuhcz_1750ia-ON0RNSAtUfTqh1cHjyG5M5NMxhRuoPwVvAzPcQQ';
 
 const API = axios.create({
-  baseURL: "https://raisav-api.devbstaging.com/api",
-  headers: {
-    "Content-Type": "application/json",
-  }
+	baseURL: 'https://raisav-api.devbstaging.com/api',
+	headers: {
+		'Content-Type': 'application/json',
+		Authorization: `Bearer ${tempToken}`,
+	},
 });
 
 export default API;
--- src/pages/explore-events/ExploreEvents.tsx
+++ src/pages/explore-events/ExploreEvents.tsx
@@ -1,23 +1,25 @@
-import { Box, Container } from '@mui/material';
+import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import EventHeader from '../../components/event-header/EventHeader';
 import ExploreEventsVM from './ExploreEventsViewModel';
 import { EventCard } from '../../components/event-card/EventCard';
-import { loadEvents } from './EventDataLoader';
 import styles from './ExploreEvents.module.css';
 import GenericButton, { ButtonTypes } from '../../components/buttons/ButtonComponent';
 import { BUTTON_STYLES } from '../../themes/styles/Button';
 
 const ExploreEvents = () => {
-	const { filters, handleFiltersChange } = ExploreEventsVM();
-	const events = loadEvents();
+	const { filters, handleFiltersChange, events, eventsLoading, eventFetchError } = ExploreEventsVM();
 
 	return (
 		<Container className={styles.container}>
 			<EventHeader filters={filters} handleFiltersChange={handleFiltersChange} />
 			<Box className={styles.eventsContainer}>
-				{events.map((event) => (
-					<EventCard key={event.id} {...event} />
-				))}
+				{eventsLoading ? (
+					<CircularProgress />
+				) : eventFetchError ? (
+					<Typography variant='body1'>{eventFetchError}</Typography>
+				) : (
+					events?.map((event) => <EventCard key={event.id} {...event} />)
+				)}
 			</Box>
 			<Box className={styles.loadMore}>
 				<GenericButton

f4067e2ca1d57670a9d551491c5cd8a0f25cc2f5
jusgri5 <justinas.gricius@gmail.com>
2024-04-24 11:06:45 +0300

    Using custom loading component instead of MUI one

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const tempToken =
-	'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndXkuaGF3a2luc0BnbWFpbC5jb20iLCJpYXQiOjE3MTM3OTQzODYsImV4cCI6MTcxMzg4MDc4Nn0._R4meTOryF5FEc9A1aRDXzjyJfd3klYl8GQuhcz_1750ia-ON0RNSAtUfTqh1cHjyG5M5NMxhRuoPwVvAzPcQQ';
+	'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndXkuaGF3a2luc0BnbWFpbC5jb20iLCJpYXQiOjE3MTM4ODU2OTEsImV4cCI6MTcxMzk3MjA5MX0.hkGEzfbTxt5ysK6RJtgW3g5zYWDBTxtpaCftIDAux5TfxZtGnzPMX4GaS5pn25TM0h-4at4i2oUD-yJ0fEeG_Q';
 
 const API = axios.create({
 	baseURL: 'https://raisav-api.devbstaging.com/api',
--- src/pages/explore-events/ExploreEvents.tsx
+++ src/pages/explore-events/ExploreEvents.tsx
@@ -1,35 +1,40 @@
-import { Box, CircularProgress, Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import EventHeader from '../../components/event-header/EventHeader';
 import ExploreEventsVM from './ExploreEventsViewModel';
 import { EventCard } from '../../components/event-card/EventCard';
 import styles from './ExploreEvents.module.css';
 import GenericButton, { ButtonTypes } from '../../components/buttons/ButtonComponent';
 import { BUTTON_STYLES } from '../../themes/styles/Button';
+import LoadingIndicator from '../../components/loading-indicator/LoadingIndicator';
 
 const ExploreEvents = () => {
 	const { filters, handleFiltersChange, events, eventsLoading, eventFetchError } = ExploreEventsVM();
 
 	return (
-		<Container className={styles.container}>
-			<EventHeader filters={filters} handleFiltersChange={handleFiltersChange} />
-			<Box className={styles.eventsContainer}>
-				{eventsLoading ? (
-					<CircularProgress />
-				) : eventFetchError ? (
-					<Typography variant='body1'>{eventFetchError}</Typography>
-				) : (
-					events?.map((event) => <EventCard key={event.id} {...event} />)
-				)}
-			</Box>
-			<Box className={styles.loadMore}>
-				<GenericButton
-					title='Load more events'
-					onClick={() => {}}
-					styles={BUTTON_STYLES.BLACK}
-					type={ButtonTypes.button}
-				/>
-			</Box>
-		</Container>
+		<>
+			{eventsLoading ? (
+				<LoadingIndicator />
+			) : (
+				<Container className={styles.container}>
+					<EventHeader filters={filters} handleFiltersChange={handleFiltersChange} />
+					<Box className={styles.eventsContainer}>
+						{eventFetchError ? (
+							<Typography variant='body1'>{eventFetchError}</Typography>
+						) : (
+							events?.map((event) => <EventCard key={event.id} {...event} />)
+						)}
+					</Box>
+					<Box className={styles.loadMore}>
+						<GenericButton
+							title='Load more events'
+							onClick={() => {}}
+							styles={BUTTON_STYLES.BLACK}
+							type={ButtonTypes.button}
+						/>
+					</Box>
+				</Container>
+			)}
+		</>
 	);
 };
 

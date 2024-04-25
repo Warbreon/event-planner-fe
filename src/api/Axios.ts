c8e69c8e8da4a3fe128de9de533eb3bd01ea5858
jusgri5 <justinas.gricius@gmail.com>
2024-04-25 09:40:59 +0300

    Removed temp auth token

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-const tempToken =
-	'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndXkuaGF3a2luc0BnbWFpbC5jb20iLCJpYXQiOjE3MTM4ODU2OTEsImV4cCI6MTcxMzk3MjA5MX0.hkGEzfbTxt5ysK6RJtgW3g5zYWDBTxtpaCftIDAux5TfxZtGnzPMX4GaS5pn25TM0h-4at4i2oUD-yJ0fEeG_Q';
-
 const API = axios.create({
 	baseURL: 'https://raisav-api.devbstaging.com/api',
 	headers: {
 		'Content-Type': 'application/json',
-		Authorization: `Bearer ${tempToken}`,
 	},
 });
 

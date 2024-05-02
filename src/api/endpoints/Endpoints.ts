0e383ac79ea1f75027225433a12725832be6ba92
Warbreon <w4rbreon@gmail.com>
2024-05-02 08:22:02 +0300

    implemented filtering by date and location functionality

--- src/api/endpoints/Endpoints.ts
+++ src/api/endpoints/Endpoints.ts
@@ -5,6 +5,7 @@ export const ENDPOINTS = {
     createNewEvent: "/events/create/new",
 
     getAllTags: "/tags",
+    getAllCities: "/addresses/cities",
 
     authenticate: "/auth/authenticate",
     refreshAccessToken: "/auth/refresh",

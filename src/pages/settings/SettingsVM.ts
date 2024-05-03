59c3bca86003985d6095b306da9ea68572b85906
jusgri5 <justinas.gricius@gmail.com>
2024-05-03 14:50:47 +0300

    Fixed admin removal so it properly informs user when there is an error and updates the table accordingly

--- src/pages/settings/SettingsVM.ts
+++ src/pages/settings/SettingsVM.ts
@@ -1,13 +1,14 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import useUserAPI from '../../api/UsersAPI';
-import { useFetch } from '../../api/hooks/ApiHooks';
+import { useFetch, usePost } from '../../api/hooks/ApiHooks';
 import { ALERT_SEVERITY } from '../../components/snackbar/SnackbarComponent';
 
 const SettingsVM = () => {
 	const [snackbarOpen, setOpen] = useState(false);
 	const [snackbarText, setText] = useState('');
 	const [snackbarSeverity, setSeverity] = useState(ALERT_SEVERITY.SUCCESS);
+	const [demotionInProgress, setDemotionInProgress] = useState(false);
 
 	const handleSnackbarClose = () => {
 		setOpen(false);
@@ -17,27 +18,41 @@ const SettingsVM = () => {
 
 	const fetchFunction = useCallback(() => {
 		return fetchAdmins();
-	}, []);
+	}, [demotionInProgress]);
 
 	const { data: adminList, isLoading, error } = useFetch(fetchFunction);
 
-	const handleRemoveClick = (id: number | string) => {
-		demoteAdmin(id)
-			.catch((error) => {
-				setText(error);
-				setSeverity(ALERT_SEVERITY.ERROR);
-				setOpen(true);
-			})
-			.then(() => {
-				if (adminList != null) {
-					const adminIndex = adminList.findIndex((admin) => admin.id === id);
-					adminList?.splice(adminIndex, 1);
-					setText('Success');
-					setSeverity(ALERT_SEVERITY.SUCCESS);
-					setOpen(true);
-				}
-			});
+	const { postData: patchData, isLoading: patchIsLoading, error: patchError } = usePost();
+
+	const handleRemoveClick = async (id: number | string) => {
+		setDemotionInProgress(true);
+		await patchData(() => demoteAdmin(id));
 	};
+
+	useEffect(() => {
+		if (!demotionInProgress) {
+			return;
+		}
+
+		if (patchIsLoading) {
+			setText('Loading...');
+			setSeverity(ALERT_SEVERITY.WARNING);
+			setOpen(true);
+			return;
+		}
+
+		if (patchError) {
+			setText(patchError.toString());
+			setSeverity(ALERT_SEVERITY.ERROR);
+			setOpen(true);
+			return;
+		}
+
+		setText('Success');
+		setSeverity(ALERT_SEVERITY.SUCCESS);
+		setOpen(true);
+		setDemotionInProgress(false);
+	}, [demotionInProgress, patchIsLoading, patchError]);
 
 	return {
 		handleRemoveClick,

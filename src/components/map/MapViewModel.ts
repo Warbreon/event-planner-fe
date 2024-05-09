cbd9b6a4fecd9b406b2eb2078f7cfe11c1e01d28
nrbtte <karolinanarbutait@gmail.com>
2024-05-09 16:57:18 +0300

    Replace Fetch with axios

--- src/components/map/MapViewModel.ts
+++ src/components/map/MapViewModel.ts
@@ -1,29 +1,30 @@
 import { useEffect, useState } from 'react';
 import { Address } from '../../models/Address';
+import axios from 'axios';
 
 const useMapViewModel = (location: Address | null | undefined) => {
 	const [position, setPosition] = useState<[number, number] | null>(null);
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(
-					`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
-						`${location?.zip} ${location?.city}, ${location?.street} ${location?.building}`
-					)}`
-				);
-				const data = await response.json();
-				if (data && data.length > 0) {
-					const { lat, lon } = data[0];
-					setPosition([parseFloat(lat), parseFloat(lon)]);
-				}
-			} catch (error) {
-				console.error('Error fetching location:', error);
-			}
-		};
-
-		fetchData();
-	}, [location?.building, location?.city, location?.street, location?.zip]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(
+            `${location?.zip} ${location?.city}, ${location?.street} ${location?.building}`
+          )}`
+        );
+        const data = response.data;
+        if (data && data.length > 0) {
+          const { lat, lon } = data[0];
+          setPosition([parseFloat(lat), parseFloat(lon)]);
+        }
+      } catch (error) {
+        console.error('Error fetching location:', error);
+      }
+    };
+		
+    fetchData();
+  }, [location?.building, location?.city, location?.street, location?.zip]);
 
 	return { position };
 };

7a145a50928b675eb5fecce44497f1ca6ff8d36d
nrbtte <karolinanarbutait@gmail.com>
2024-05-09 16:40:54 +0300

    Add navigation to google maps

--- src/components/map/MapSection.tsx
+++ src/components/map/MapSection.tsx
@@ -2,15 +2,23 @@ import { FC } from 'react';
 import { Address } from '../../models/Address';
 import Map from './Map';
 import SectionHeader from '../../shared/components/section-header/SectionHeader';
+import { constructGoogleMapsURL } from '../../utils/MapsUrlByAddress';
 
 interface MapSectionProps {
 	address: Address | null;
 }
 
 const MapSection: FC<MapSectionProps> = ({ address }) => {
+	if (!address) {
+    return <section>No address provided</section>;
+  }
+
+	const {venueName, city, street, building, zip} = address;
+	const url = constructGoogleMapsURL(venueName, city, street, building, zip);
+	
 	return (
 		<section>
-			<SectionHeader name='Location' buttonTitle='View in maps' onButtonClick={() => {}} />
+			<SectionHeader name='Location' buttonTitle='View in maps' onButtonClick={() => {window.location.href = url}} />
 			<Map location={address} />
 		</section>
 	);

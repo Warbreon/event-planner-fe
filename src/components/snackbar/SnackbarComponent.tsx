f90be770fd30b0dc61d999a17bef2f26ee90611a
Apozzz <bosas.arnas2000@gmail.com>
2024-05-16 18:04:50 +0300

    fix: small cleanup

--- src/components/snackbar/SnackbarComponent.tsx
+++ src/components/snackbar/SnackbarComponent.tsx
@@ -2,35 +2,45 @@ import { FC } from 'react';
 import { Alert, AlertProps, Snackbar } from '@mui/material';
 
 export enum ALERT_SEVERITY {
-	ERROR = 'error',
-	SUCCESS = 'success',
-	WARNING = 'warning',
+    ERROR = 'error',
+    SUCCESS = 'success',
+    WARNING = 'warning',
 }
 
 interface SnackBarProps {
-	open: boolean;
-	message: string;
-	severity: ALERT_SEVERITY;
-	autoHideDuration?: number;
-	handleClose?: (event?: React.SyntheticEvent | Event, reason?: string) => void;
-	className?: string;
+    open: boolean;
+    message: string;
+    severity: ALERT_SEVERITY;
+    autoHideDuration?: number;
+    handleClose?: (event?: React.SyntheticEvent | Event, reason?: string) => void;
+    className?: string;
 }
-const SnackbarComponent: FC<SnackBarProps> = ({ open, handleClose, message, severity, autoHideDuration, className }) => {
-	const alertProps: AlertProps = {
+
+const SnackbarComponent: FC<SnackBarProps> = ({
+    open,
+    handleClose,
+    message,
+    severity,
+    autoHideDuration = 5000,
+    className,
+}) => {
+    const alertProps: AlertProps = {
         severity,
+        onClose: autoHideDuration ? undefined : handleClose,
     };
-	
-	if (!autoHideDuration) {
-        alertProps.onClose = handleClose;
-    }
-	
-	return (
-		<Snackbar open={open} autoHideDuration={autoHideDuration} onClose={handleClose} className={className}>
-			<Alert {...alertProps}>
-				{message}
-			</Alert>
-		</Snackbar>
-	);
+
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={autoHideDuration}
+            onClose={handleClose}
+            className={className}
+        >
+            <Alert {...alertProps}>
+                {message}
+            </Alert>
+        </Snackbar>
+    );
 };
 
 export default SnackbarComponent;

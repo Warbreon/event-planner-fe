097db6fe89700c0c976abce333b75a05d5da8589
nrbtte <karolinanarbutait@gmail.com>
2024-05-17 10:13:13 +0300

    Remove commented out code blocks

--- src/utils/mappings/EventMappings.ts
+++ src/utils/mappings/EventMappings.ts
@@ -1,10 +1,10 @@
 import { EventFormValues } from "../../interfaces/EventFormValuesInterface";
 import { formatAgendaItems, parseAgendaItems } from "../AgendaUtils";
 import { combineDateTime, splitDateTime } from "../DateConverter";
-import { EventRequest } from "../../models/request/EventRequest";
 import { LocationTags } from "../../constants/LocationTags";
 import { Currency } from "../../constants/Currency";
 import { getBase64 } from "../Base64Encoder";
+import {Event} from '../../models/Event';
 
 export const mapEventFormValuesToEvent =  async (formValues: EventFormValues) => {
     const imageBase64 = formValues.imageBase64 ?  await getBase64(formValues.imageBase64 as unknown as File) : null;
@@ -30,97 +30,46 @@ export const mapEventFormValuesToEvent =  async (formValues: EventFormValues) =>
     };
 };
 
-// export const mapEventToFormValues = async (event: EventRequest) => {
+export const mapEventToFormValues = (event: Event) => {
 
-//     const determineLocationKey = () => {
-//         if (event.inviteUrl) {
-//             return LocationTags.ONLINE;
-//         } else if (event.addressId) {
-//             return LocationTags.PHYSICAL;
-//         } else {
-//             return LocationTags.TBD;
-//         }
-//     };
+    const determineLocationKey = () => {
+        if (event.inviteUrl) {
+            return LocationTags.ONLINE;
+        } else if (event.address?.id) {
+            return LocationTags.PHYSICAL;
+        } else {
+            return LocationTags.TBD;
+        }
+    };
 
-//     const { date: eventStartDate, time: eventStartTime } = splitDateTime(event.eventStart);
-//     const { date: eventEndDate, time: eventEndTime } = splitDateTime(event.eventEnd);
-//     const { date: registrationStartDate, time: registrationStartTime } = splitDateTime(event.registrationStart);
-//     const { date: registrationEndDate, time: registrationEndTime } = splitDateTime(event.registrationEnd);
-
-//     const imageBase64 = event.imageBase64 ? await getBase64(event.imageBase64 as unknown as File) : null;
-//     const cardImageBase64 = event.cardImageBase64 ?  await getBase64(event.cardImageBase64 as unknown as File) : null;
+    const { date: eventStartDate, time: eventStartTime } = splitDateTime(event.eventStart);
+    const { date: eventEndDate, time: eventEndTime } = splitDateTime(event.eventEnd);
+    const { date: registrationStartDate, time: registrationStartTime } = splitDateTime(event.registrationStart);
+    const { date: registrationEndDate, time: registrationEndTime } = splitDateTime(event.registrationEnd);
 
 
-
-//     return {
-//         eventName: event.name,
-//         eventStartDate: eventStartDate,
-//         eventStartTime: eventStartTime,
-//         eventEndDate: eventEndDate,
-//         eventEndTime: eventEndTime,
-//         registrationStartDate: registrationStartDate,
-//         registrationStartTime: registrationStartTime,
-//         registrationEndDate: registrationEndDate,
-//         registrationEndTime: registrationEndTime,
-//         agenda: event.agenda ? parseAgendaItems(event.agenda) : [],
-//         imageBase64: imageBase64, 
-//         cardImageBase64: cardImageBase64, 
-//         isOpen: event.isOpen,
-//         addressId: event.addressId,
-//         inviteUrl: event.inviteUrl ?? null,
-//         attendeeIds: event.attendeeIds,
-//         eventTagIds: event.tagIds,
-//         locationKey: determineLocationKey(),
-//         currency: event.currency ?? Currency.EUR,
-//         tickets: event.tickets,
-//         description: event.description,
-//         price: event.price
-//     };
-// };
-
-// export const mapEventToFormValues = (event: Event): EventFormValues => {
-//     const determineLocationKey = () => {
-//         if (event.inviteUrl) {
-//             return LocationTags.ONLINE;
-//         } else if (event.address?.id) {
-//             return LocationTags.PHYSICAL;
-//         } else {
-//             return LocationTags.TBD;
-//         }
-//     };
-
-//     const { date: eventStartDate, time: eventStartTime } = splitDateTime(event.eventStart);
-//     const { date: eventEndDate, time: eventEndTime } = splitDateTime(event.eventEnd);
-//     const { date: registrationStartDate, time: registrationStartTime } = splitDateTime(event.registrationStart);
-//     const { date: registrationEndDate, time: registrationEndTime } = splitDateTime(event.registrationEnd);
-
-//     const imageBase64 = values.imageBase64 ? await getBase64(values.imageBase64 as unknown as File) : null;
-//     const cardImageBase64 = values.cardImageBase64 ? await getBase64(values.cardImageBase64 as unknown as File) : null;
-
-
-
-//     return {
-//         eventName: event.name,
-//         eventStartDate: eventStartDate,
-//         eventStartTime: eventStartTime,
-//         eventEndDate: eventEndDate,
-//         eventEndTime: eventEndTime,
-//         registrationStartDate: registrationStartDate,
-//         registrationStartTime: registrationStartTime,
-//         registrationEndDate: registrationEndDate,
-//         registrationEndTime: registrationEndTime,
-//         agenda: event.agenda ? parseAgendaItems(event.agenda) : [],
-//         imageBase64: null, 
-//         cardImageBase64: null, 
-//         isOpen: event.isOpen,
-//         addressId: event.address ? event.address.id : null,
-//         inviteUrl: event.inviteUrl ?? null,
-//         attendeeIds: event.attendees ? event.attendees.map(att => att.id) : [],
-//         eventTagIds: event.tags ? event.tags.map(tag => tag.id) : [],
-//         locationKey: determineLocationKey(),
-//         currency: event.currency ?? Currency.USD,
-//         tickets: event.tickets,
-//         description: event.description,
-//         price: event.price
-//     };
-// };
+    return {
+        eventName: event.name,
+        eventStartDate: eventStartDate,
+        eventStartTime: eventStartTime,
+        eventEndDate: eventEndDate,
+        eventEndTime: eventEndTime,
+        registrationStartDate: registrationStartDate,
+        registrationStartTime: registrationStartTime,
+        registrationEndDate: registrationEndDate,
+        registrationEndTime: registrationEndTime,
+        agenda: event.agenda ? parseAgendaItems(event.agenda) : [],
+        imageBase64: null, 
+        cardImageBase64: null, 
+        isOpen: event.isOpen,
+        addressId: event.address ? event.address.id : null,
+        inviteUrl: event.inviteUrl ?? null,
+        attendeeIds: event.attendees ? event.attendees.map(att => att.id) : [],
+        eventTagIds: event.tags ? event.tags.map(tag => tag.id) : [],
+        locationKey: determineLocationKey(),
+        currency: event.currency ?? Currency.USD,
+        tickets: event.tickets,
+        description: event.description,
+        price: event.price
+    };
+};

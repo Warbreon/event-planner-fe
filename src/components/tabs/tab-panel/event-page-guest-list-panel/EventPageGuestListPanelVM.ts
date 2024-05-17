5004d03643c92399f51b8ccd5f47f685f5b9a0c1
Vladislav Legenzov <w4rbreon@gmail.com>
2024-05-17 12:54:24 +0300

    added debounced filtering

--- src/components/tabs/tab-panel/event-page-guest-list-panel/EventPageGuestListPanelVM.ts
+++ src/components/tabs/tab-panel/event-page-guest-list-panel/EventPageGuestListPanelVM.ts
@@ -3,6 +3,7 @@ import useAttendeeAPI from '../../../../api/AttendeeAPI';
 import { BUTTON_STYLES } from '../../../../themes/styles/Button';
 import { useApiRequest } from '../../../../api/hooks/ApiHooks';
 import { Attendee } from '../../../../models/Attendee';
+import { useDebouncedCallback } from 'use-debounce';
 
 const EventPageGuestListPanelVM = (attendees: Attendee[]) => {
     const [lastClicked, setLastClicked] = useState<Record<number, string>>({});
@@ -11,35 +12,37 @@ const EventPageGuestListPanelVM = (attendees: Attendee[]) => {
     const { confirmPendingRegistration, declinePendingRegistration } = useAttendeeAPI();
     const { request: patchData } = useApiRequest();
 
-    useEffect(() => {
-        const term = searchTerm.toLowerCase();
+    const searchAttendees = (value: string) => {
+        const term = value.toLowerCase();
         setFilteredAttendees(
-            attendees.filter(attendee => 
+            attendees.filter(attendee =>
                 `${attendee.user.firstName} ${attendee.user.lastName}`.toLowerCase().includes(term)
             )
-        )
-    }, [searchTerm, attendees]);
+        );
+    };
+
+    const debouncedSearchAttendees = useDebouncedCallback(searchAttendees, 500);
 
     const onPlusButtonClick = () => {
         (console.log("Button clicked"));
     };
+
     const onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(event.target.value);
-        console.log("New input", event.target.value);
+        const value = event.target.value;
+        setSearchTerm(value);
+        debouncedSearchAttendees(value);
     };
     const onConfirmClick = async (attendeeId: number) => {
         if (lastClicked[attendeeId] !== 'confirm') {   
             setLastClicked(prevState => ({ ...prevState, [attendeeId]: 'confirm' }));
-            //console.log("Confirmed", attendeeId);
-            //await patchData(() => confirmPendingRegistration(attendeeId));
+            await patchData(() => confirmPendingRegistration(attendeeId));
         }
     };
 
     const onDeclineClick = async (attendeeId: number) => {
         if (lastClicked[attendeeId] !== 'decline') {    
             setLastClicked(prevState => ({ ...prevState, [attendeeId]: 'decline' }));
-            //console.log("Declined", attendeeId);
-            //await patchData(() => declinePendingRegistration(attendeeId));
+            await patchData(() => declinePendingRegistration(attendeeId));
         }
     };
 

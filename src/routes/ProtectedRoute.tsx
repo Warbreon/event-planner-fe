614a5bbd4b2c72275520658e09f12961b1b69c6a
nrbtte <karolinanarbutait@gmail.com>
2024-05-18 15:43:01 +0300

    Fix access token refresh

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,27 +2,17 @@ import ROUTES from './Routes';
 import { FC, ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import useProtectedRouteVM from './ProtectedRouteVM';
-import LoadingIndicator from '../components/loading-indicator/LoadingIndicator';
 
 interface ProtectedRouteProps {
 	children: ReactNode;
 }
 
 const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
-	const { isUserAuthenticated, isRefreshTokenExpired, isLoading } = useProtectedRouteVM();
+	const { isUserAuthenticated, isRefreshTokenExpired } = useProtectedRouteVM();
 
-	if (!isUserAuthenticated) {
+	if (isRefreshTokenExpired || !isUserAuthenticated) {
 		return <Navigate to={ROUTES.SIGN_IN} />;
 	}
-
-	if(isRefreshTokenExpired) {
-		return <Navigate to={ROUTES.SIGN_IN} />;
-	}
-
-	if(isLoading) {
-		return <LoadingIndicator/>
-	}
-
 	return <>{children}</>;
 };
 
--- src/routes/ProtectedRouteVM.ts
+++ src/routes/ProtectedRouteVM.ts
@@ -1,58 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { StoreState } from '../redux/store/Store';
 import { isExpired } from 'react-jwt';
-import { useApiRequest } from '../api/hooks/ApiHooks';
-import { useNavigate } from 'react-router';
-import { refreshAccessToken, signOut } from '../redux/slices/AuthenticationSlice';
-import { useCallback, useEffect, useState } from 'react';
-import useAuthenticationAPI from '../api/AuthenticationAPI';
-import ROUTES from './Routes';
-import { removeUserInfo } from '../redux/slices/UserInfoSlice';
 
 const useProtectedRouteVM = () => {
 	const isUserAuthenticated = useSelector((state: StoreState) => state.user.signedIn);
-	const currentAccessToken = useSelector((state: StoreState) => state.user.accessToken);
 	const refreshToken = useSelector((state: StoreState) => state.user.refreshToken);
-
-	const isAccessTokenExpired = isExpired(currentAccessToken);
 	const isRefreshTokenExpired = isExpired(refreshToken);
 
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-
-	const { refresh } = useAuthenticationAPI();
-	const { request: postData, data, error, isLoading } = useApiRequest();
-
-	const [fetchingNewToken, setFetchingNewToken] = useState(false);
-	const fetchNewAccessToken = useCallback(async () => {
-		if (fetchingNewToken || isLoading) return;
-		setFetchingNewToken(true);
-		await postData(() => refresh(refreshToken));
-		setFetchingNewToken(false);
-	}, [fetchingNewToken, isLoading, postData, refresh, refreshToken]);
-
-	useEffect(() => {
-		if (isAccessTokenExpired && !isRefreshTokenExpired && !fetchingNewToken) {
-			fetchNewAccessToken();
-		}
-	}, [isAccessTokenExpired, isRefreshTokenExpired, fetchNewAccessToken, fetchingNewToken]);
-
-	useEffect(() => {
-		if (data && data.accessToken !== null && !fetchingNewToken) {
-			console.log(data.accessToken);
-			dispatch(refreshAccessToken(data.accessToken));
-		}
-	}, [data, dispatch, fetchingNewToken]);
-
-	useEffect(() => {
-		if (isRefreshTokenExpired) {
-			dispatch(signOut());
-			dispatch(removeUserInfo());
-			navigate(ROUTES.SIGN_IN, { replace: true });
-		}
-	}, [isRefreshTokenExpired, dispatch, navigate]);
-
-	return { isUserAuthenticated, isRefreshTokenExpired, isLoading };
+	return { isUserAuthenticated, isRefreshTokenExpired};
 };
 
 export default useProtectedRouteVM;

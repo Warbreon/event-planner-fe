fac551156536118db1cf434a1c9a1bb3f75a6828
nrbtte <karolinanarbutait@gmail.com>
2024-05-26 14:41:15 +0300

    Fix issue navigating to page not found on sign out from edit event and admins pages

--- src/routes/CreateEditEventProtectedRoute.tsx
+++ src/routes/CreateEditEventProtectedRoute.tsx
@@ -1,18 +1,19 @@
-import { useSelector } from 'react-redux';
 import { FC, ReactNode } from 'react';
 import ROUTES from './Routes';
 import { Navigate } from 'react-router-dom';
+import { UserRoles } from '../utils/PermissionParser';
+import useProtectedRouteVM from './ProtectedRouteVM';
 
 interface ProtectedRouteProps {
 	children: ReactNode;
 }
 
 const CreateEditEventProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
-	const userRole = useSelector((state: any) => state.user.role);
+	const { isUserAuthenticated, userRole } = useProtectedRouteVM();
 
-	if (userRole !== 'SYSTEM_ADMIN' && userRole !== 'EVENT_ADMIN') {
+	if (userRole !== UserRoles.SYSTEM_ADMIN && userRole !== UserRoles.EVENT_ADMIN && isUserAuthenticated) {
 		return <Navigate to={ROUTES.NOT_FOUND}/>;
-	}
+	} 
 
 	return <>{children}</>;
 };
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -65,7 +65,9 @@ const router = createBrowserRouter([
 				path: ROUTES.SETTINGS,
 				element: (
 					<SystemAdminRoute>
-						<Settings />
+						<ProtectedRoute>
+							<Settings />
+						</ProtectedRoute>
 					</SystemAdminRoute>
 				),
 			},

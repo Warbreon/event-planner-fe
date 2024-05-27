e1a697c665168a81e808a2c27f84dbebbfe61928
Edvinas <klevinskas.edvinas@gmail.com>
2024-05-27 11:59:22 +0300

    change api from patch to get

--- src/api/AttendeeAPI.ts
+++ src/api/AttendeeAPI.ts
@@ -9,7 +9,7 @@ const useAttendeeAPI = () => {
     const confirmPendingRegistration = (attendeeId: number) => axiosInstance.patch(ENDPOINTS.confirmPendingRegistration(attendeeId));
     const declinePendingRegistration = (attendeeId: number) => axiosInstance.patch(ENDPOINTS.declinePendingRegistration(attendeeId));
     const updateEventAttendees = (eventId: number, userId: number[]) => axiosInstance.patch(ENDPOINTS.updateEventAttendees(eventId), userId);
-    const getEventAttendees = (eventId: number) => axiosInstance.patch<Attendee[]>(ENDPOINTS.getEventAttendees(eventId));
+    const getEventAttendees = (eventId: number) => axiosInstance.get<Attendee[]>(ENDPOINTS.getEventAttendees(eventId));
 
 
     return { fetchNotifications, markNotificationAsViewed, confirmPendingRegistration, declinePendingRegistration, updateEventAttendees, getEventAttendees };
